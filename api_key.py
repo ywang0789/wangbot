0ccc1f54667971d6e1f90821b71dd26480f8603c
ywang0789 <ywang0789@conestogac.on.ca>
2024-01-24 14:43:10 -0500

    added gitignore

--- api_key.py
+++
@@ -1,8 +0,0 @@
-import json 
-
-with open("hidden/api_keys.json", "r") as file:
-    keys = json.load(file)
-
-gpt_api_key = keys["gpt_api_key"]
-elevenlabs_api_key = keys["elevenlabs_api_key"]
-discord_api_key = keys["discord_api_key"]
---
+++ api_keys.py
@@ -0,0 +1,10 @@
+import json 
+
+with open("hidden/keys.json", "r") as file:
+    keys = json.load(file)
+
+gpt_api_key = keys["gpt_api_key"]
+elevenlabs_api_key = keys["elevenlabs_api_key"]
+discord_public_key = keys["discord_public_key"]
+discord_app_id = keys["discord_app_id"]
+discord_token = keys["discord_token"]
