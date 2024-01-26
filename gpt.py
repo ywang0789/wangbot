5e715f6b03d3445d8c505a3cdcaaa738425d152c
ywang0789 <ywang0789@conestogac.on.ca>
2024-01-26 06:28:32 -0500

    fomatting

--- gpt.py
+++ gpt.py
@@ -10,23 +10,22 @@ import time
 API_KEY = api_keys.gpt_api_key
 
 # language model settings
-LANG_MODEL = "gpt-4-0125-preview" # "gpt-4-0125-preview" ($0.01 / 1K tokens) OR "gpt-3.5-turbo-1106" ($0.0010 / 1K tokens)
+LANG_MODEL = 'gpt-4-0125-preview'  # "gpt-4-0125-preview" ($0.01 / 1K tokens) OR "gpt-3.5-turbo-1106" ($0.0010 / 1K tokens)
 TEXT_PRICING_RATE = 0.0010 / 1000  # change if change model
 
 # image model settings
-IMG_MODEL = "dall-e-3" # "dall-e-3" ($0.040 / image) OR "dall-e-2 ($0.020 / image")
-IMG_SIZE = "1024x1024"
-IMG_PRICING_RATE = 0.020 # change if change model
-IMG_QUALITY = "standard"  
+IMG_MODEL = 'dall-e-3'  # "dall-e-3" ($0.040 / image) OR "dall-e-2 ($0.020 / image")
+IMG_SIZE = '1024x1024'
+IMG_PRICING_RATE = 0.020  # change if change model
+IMG_QUALITY = 'standard'
 
+# system prompts
 CONSTANT_SYSTEM_PROMPT = """
 You will be speaking with muliple people, their names will be indicated in the message.
 the format is:
 <name>: <message>
 Your responses do not need to follow this format.
-
 """
-
 SYSTEM_PROMPT = prompts.SYSTEM_PROMPT1  # hehe ;)
 
 
@@ -40,14 +39,15 @@ class gpt:
         # init history for saving context
         self.history = []
         # set system prompt to history
-        self.history.append({"role": "system", "content": CONSTANT_SYSTEM_PROMPT})
-        self.history.append({"role": "system", "content": SYSTEM_PROMPT})
+        self.history.append({'role': 'system', 'content': CONSTANT_SYSTEM_PROMPT})
+        self.history.append({'role': 'system', 'content': SYSTEM_PROMPT})
 
         # session usage
-        self.usage = 0
+        self.usage = 0.00
 
     def get_text_response(self, user_input: str) -> str:
         """Returns a response from GPT. Also updates the history."""
+
         user_input_len = len(user_input)
         if user_input_len == 0:
             return "Please enter something."
@@ -56,7 +56,7 @@ class gpt:
             return "Please keep your input under 500 characters."
 
         # add user message to history
-        self.history.append({"role": "user", "content": user_input})
+        self.history.append({'role': 'user', 'content': user_input})
         # get completion
         response = self.client.chat.completions.create(
             model=LANG_MODEL, messages=self.history
@@ -65,60 +65,65 @@ class gpt:
         response_content = response.choices[0].message.content
 
         # add response to history
-        self.history.append({"role": "assistant", "content": response_content})
+        self.history.append({'role': 'assistant', 'content': response_content})
 
         # update session usage
         self.usage += response.usage.total_tokens * TEXT_PRICING_RATE
 
         return response_content
 
-    def append_system_prompt(self, new_system_prompt: str):
+    def append_system_prompt(self, new_system_prompt: str) :
         """Appends a new system prompt to the history."""
-        self.history.append({"role": "assistant", "content": new_system_prompt})
-
-        return "System prompt appended."
+        self.history.append({'role': 'assistant', 'content': new_system_prompt})
 
     def overwrite_system_prompt(self, new_system_prompt: str):
         """Sets the system prompt to the new system prompt."""
-        self.history[0] = {"role": "system", "content": new_system_prompt}
+        self.history[0] = {'role': 'system', 'content': new_system_prompt}
 
-        print(self.history)
-
-        return "system prompt overwritten."
-
-    def get_usage(self):
+    def get_usage(self) -> float:
         """Returns the session usage."""
         return self.usage
 
-    def save_history(self, file_name: str = "history.json") -> bool:
+    def save_history_to(self, file_name: str = 'history.json') -> bool:
         """Saves the history to a json file."""
         try:
-            with open(file_name, "w") as file:
+            with open(file_name, 'w') as file:
                 json.dump(self.history, file)
         except FileNotFoundError:
             return False
 
         return True
 
-    def load_history(self, file_name: str = "history.json") -> bool:
+    def load_history_from(self, file_name: str = 'history.json') -> bool:
         """Loads the history from a json file."""
         try:
-            with open(file_name, "r") as file:
+            with open(file_name, 'r') as file:
                 self.history = json.load(file)
         except FileNotFoundError:
             return False
 
         return True
 
-    def clear_history(self):
+    def soft_reset_history(self):
         """Clears the history, except for the system prompts."""
         self.history = self.history[0:2]
+    
+    def hard_reset_history(self):
+        """Clears the history, system prompts set as defaults."""
+        self.history = []
+        self.history.append({'role': 'system', 'content': CONSTANT_SYSTEM_PROMPT})
+        self.history.append({'role': 'system', 'content': SYSTEM_PROMPT})
+
+    def total_reset_history(self):
+        """Clears ALL history"""
+        self.history = []
 
     def get_history(self) -> list:
-        """Returns the history. (does not include system prompts)"""
+        """Returns the history. (does not include system prompts) :)"""
         return self.history[2:]
 
     def get_image_response(self, user_input: str) -> str:
+        """Returns an image response from GPT."""
         response = self.client.images.generate(
             model=IMG_MODEL,
             prompt=user_input,
@@ -131,7 +136,7 @@ class gpt:
         return image_url
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     b = gpt()
-    #print(b.get_text_response("tim: Hello, what is your name?"))
+    # print(b.get_text_response("tim: Hello, what is your name?"))
     print(b.get_image_response("picutre of a cat"))
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ TOKEN = api_keys.discord_token
 INTENTS = discord.Intents.default()
 INTENTS.messages = True
 INTENTS.message_content = True
-BOT = commands.Bot(command_prefix="!", intents=INTENTS)
+BOT = commands.Bot(command_prefix='!', intents=INTENTS)
 
 # gpt setup
 GPT = gpt.gpt()
@@ -18,49 +18,62 @@ START_TIME = time.time()
 FORMATTED_START_TIME = time.strftime(
     "%a, %d %b %Y %H:%M:%S", time.localtime(START_TIME)
 )
-HIDDEN_FILE_DIR = "./secret/"
+HIDDEN_FILE_DIR = './secret/'
 
 
 # on start
 @BOT.event
 async def on_ready():
+    """Prints when bot is ready."""
     print(f"{BOT.user} has connected to Discord!")
+
+# on message
+@BOT.event
+async def on_message(message):
+    if message.author == BOT.user:
+        return
+    await BOT.process_commands(message)
 
 
 # !hello
-@BOT.command(name="hi", help="Says hi to you.")
+@BOT.command(name='hi', help="Says hi to you.")
 async def hi_command(ctx):
+    """Says hi to message author."""
     print(f"{ctx.message.created_at}:{ctx.author.name}: !hi.")
     await ctx.send(f"Hi, {ctx.author.name}!")
 
 
 # !wangbot
-@BOT.command(name="wangbot", help="Talk to Wangbot.")
+@BOT.command(name='wangbot', help="Talk to Wangbot.")
 async def wangbot_command(ctx, *, message: str):
+    """Get text response from Wangbot GPT."""
     print(f"{ctx.message.created_at}:{ctx.author.name}: !wangbot {message}")
     message = ctx.author.name + ": " + message
     await ctx.send(GPT.get_text_response(message))
 
 
 # !appendwang
-@BOT.command(name="appendwang", help="Appends a new system prompt.")
+@BOT.command(name='appendwang', help="Appends a new system prompt.")
 async def appendwang_command(ctx, *, message: str):
+    """Appends a new system prompt to end of history."""
     print(f"{ctx.message.created_at}:{ctx.author.name}: !appendwang {message}")
     GPT.append_system_prompt(message)
     await ctx.send("System prompt appened.")
 
 
 # !overwritewang
-@BOT.command(name="overwritewang", help="Overwrites the system prompt.")
+@BOT.command(name='overwritewang', help="Overwrites the system prompt with given message.")
 async def overwritewang_command(ctx, *, message: str):
+    """Overwrites the system prompt with message."""
     print(f"{ctx.message.created_at}:{ctx.author.name}: !overwritewang {message}")
     GPT.overwrite_system_prompt(message)
     await ctx.send("System prompt overwritten.")
 
 
 # !wanginfo
-@BOT.command(name="wanginfo", help="Gets info about current session.")
+@BOT.command(name='wanginfo', help="Gets info about current session.")
 async def usage_command(ctx):
+    """Gets info about current session."""
     print(f"{ctx.message.created_at}:{ctx.author.name}: !wanginfo.")
     # calculate time elapsed
     time_elapsed = time.time() - START_TIME
@@ -86,17 +99,18 @@ async def usage_command(ctx):
 
 
 # !savewang
-@BOT.command(name="savewang", help="Saves current history to a json file.")
+@BOT.command(name='savewang', help="Saves current history to a json file name.")
 async def save_command(ctx, *, file_name):
+    """Saves current history to a json file name."""
     print(f"{ctx.message.created_at}:{ctx.author.name}: !save {file_name}")
     # check if file name is valid
     if len(file_name) == 0:
         await ctx.send("Please enter a file name.")
         return
     # format file name
-    file_name = HIDDEN_FILE_DIR + file_name + ".json"
+    file_name = HIDDEN_FILE_DIR + file_name + '.json'
     # save history
-    if not GPT.save_history(file_name):
+    if not GPT.save_history_to(file_name):
         await ctx.send(f"Could not save history to {file_name}.")
         return
 
@@ -104,39 +118,51 @@ async def save_command(ctx, *, file_name):
 
 
 # !loadwang
-@BOT.command(name="loadwang", help="Loads the history from a json file.")
+@BOT.command(name='loadwang', help="Loads the history from a json file name.")
 async def load_command(ctx, *, file_name):
+    """Loads the history from a json file name."""
     print(f"{ctx.message.created_at}:{ctx.author.name}: !load {file_name}")
     # check if file name is valid
     if len(file_name) == 0:
         await ctx.send("Please enter a file name.")
         return
     # format file name
-    file_name = HIDDEN_FILE_DIR + file_name + ".json"
+    file_name = HIDDEN_FILE_DIR + file_name + '.json'
 
     # load history
-    if not GPT.load_history(file_name):
+    if not GPT.load_history_from(file_name):
         await ctx.send(f"Could not load history from {file_name}.")
         return
 
     await ctx.send(f"History loaded from {file_name}.")
 
 
-# !reset
+# !softresetwang
 @BOT.command(
-    name="softresetwang", help="Resets the history (keeps initial system prompt)."
+    name='softresetwang', help="Resets the history (keeps initial system prompt)."
 )
 async def softreset_command(ctx):
+    """Resets the history (keeps initial system prompt)."""
     print(f"{ctx.message.created_at}:{ctx.author.name}: !softreset.")
-    GPT.clear_history()
-    await ctx.send("History reset.")
+    GPT.soft_reset_history()
+    await ctx.send("History has been reset softly.")
 
+# !hardresetwang
+@BOT.command(
+    name='hardresetwang', help="Resets the history (sets system prompt to default)."
+)
+async def hardreset_command(ctx):
+    """Resets the history (sets system prompt to default)."""
+    print(f"{ctx.message.created_at}:{ctx.author.name}: !hardreset.")
+    GPT.hard_reset_history()
+    await ctx.send("History has been reset hardly.")
 
 # !showwang
 @BOT.command(
-    name="showwang", help="Shows the current history (does not include system prompts)."
+    name='showwang', help="Shows the current history (does not include system prompts)."
 )
 async def show_command(ctx):
+    """Shows the current history (does not include system prompts)."""
     print(f"{ctx.message.created_at}:{ctx.author.name}: !showwang.")
 
     if len(GPT.get_history()) == 0:
@@ -150,18 +176,11 @@ async def show_command(ctx):
 
 
 # !wangpic
-@BOT.command(name="wangpic", help="Generate image from a prompt.")
+@BOT.command(name='wangpic', help="Generate image from a prompt.")
 async def wangimg_command(ctx, *, message: str):
+    """Generate image from a prompt."""
     print(f"{ctx.message.created_at}:{ctx.author.name}: !wangpic {message}")
     await ctx.send(GPT.get_image_response(message))
-
-
-# on message
-@BOT.event
-async def on_message(message):
-    if message.author == BOT.user:
-        return
-    await BOT.process_commands(message)
 
 
 BOT.run(TOKEN)
