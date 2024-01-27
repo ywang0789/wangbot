e42ad06ac1e846d2a5d4695b05a877f6d3050f46
ywang0789 <ywang0789@conestogac.on.ca>
2024-01-27 04:35:10 -0500

    added util to generate random str for making unque file name

--- utils.py
+++ utils.py
@@ -1,4 +1,16 @@
 import random
+import time
+
+from datetime import datetime
+
+def get_unique_str() -> str:
+    """Returns a unique string from current datetime."""
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S%f")
+    return f"{timestamp}"
 
 def get_random_int():
     return random.randint(0, 10)
+
+if __name__ == "__main__":
+    print(str(int(time.time())))
+    print(get_unique_str())
