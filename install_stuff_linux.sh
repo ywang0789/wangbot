feaab61cecfb48d33b1068083176598086569ae4
wang <ywang0789@conestogac.on.ca>
2024-10-22 12:23:51 -0400

    added new commands for setting up machine

--- install_stuff_linux.sh
+++ install_stuff_linux.sh
@@ -5,7 +5,7 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-apt-get update
+apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y 
 
 # git
 apt-get install -y git
@@ -15,6 +15,12 @@ apt-get install -y python3 python3-pip python3-venv
 
 # tmux <3
 apt-get install -y tmux
+
+git clone https://github.com/ywang0789/wangbot.git
+git pull origin master
+python3 -m venv wangbot/venv
+source wangbot/venv/bin/activate
+pip3 install -r wangbot/requirements.txt
 
 
 
